de3f46c2b02eb04cd870d37e1008030de61b951a
DESKTOP-ENMOEDB\Toni <skuezyt@gmail.com>
2021-03-24 19:47:00 +0100

    commit2

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,22 +1,8 @@
-import React from "react";
+/*import React from "react";*/
 /*import Tilt from "react-tilt";*/
 
 const Logo = () => {
-  return (
-    <div className="ma4 mt0">
-      {
-        {
-          /* <Tilt
-        className="Tilt"
-        options={{ max: 25 }}
-        style={{ height: 250, width: 250 }}
-      >
-        <div className="Tilt-inner"> 👽 </div>
-      </Tilt> */
-        }
-      }
-    </div>
-  );
+  return <div className="ma4 mt0"></div>;
 };
 
 export default Logo;
